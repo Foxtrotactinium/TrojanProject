c5c1425000d0fa8b68e66b8be941dbbea7f570e5
Shreyas Walvekar <shreyas.whis@gmail.com>
2020-07-28 19:59:28 +1000

    qr-test-2

--- static/js/qrCodeReaderMy.js
+++ static/js/qrCodeReaderMy.js
@@ -1,4 +1,4 @@
-const qrcode = window.qrcode;
+// const qrcode = window.qrcode;
 
 const video = document.createElement("video");
 const canvasElement = document.getElementById("qr-canvas");
@@ -11,48 +11,48 @@ const btnScanQR = document.getElementById("btn-scan-qr");
 let scanning = false;
 
 btnScanQR.onclick = () =>
-  navigator.mediaDevices
-    .getUserMedia({ video: { facingMode: "environment" } })
-    .then(function(stream) {
-      scanning = true;
-      qrResult.hidden = true;
-      btnScanQR.hidden = true;
-      canvasElement.hidden = false;
-      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-      video.srcObject = stream;
-      video.play();
-      tick();
-      scan();
-    });
+    navigator.mediaDevices
+        .getUserMedia({ video: { facingMode: "environment" } })
+        .then(function(stream) {
+            scanning = true;
+            qrResult.hidden = true;
+            btnScanQR.hidden = true;
+            canvasElement.hidden = false;
+            video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+            video.srcObject = stream;
+            video.play();
+            tick();
+            scan();
+        });
 
 function tick() {
-  canvasElement.height = video.videoHeight;
-  canvasElement.width = video.videoWidth;
-  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+    canvasElement.height = video.videoHeight;
+    canvasElement.width = video.videoWidth;
+    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
 
-  scanning && requestAnimationFrame(tick);
+    scanning && requestAnimationFrame(tick);
 }
 
 function scan() {
-  try {
-    qrcode.decode();
-  } catch (e) {
-    setTimeout(scan, 300);
-  }
+    try {
+        qrcode.decode();
+    } catch (e) {
+        setTimeout(scan, 300);
+    }
 }
 
 
-    qrcode.callback = (res) => {
-      if (res) {
+qrcode.callback = (res) => {
+    if (res) {
         outputData.innerText = res;
         scanning = false;
 
         video.srcObject.getTracks().forEach(track => {
-          track.stop();
+            track.stop();
         });
 
         qrResult.hidden = false;
         btnScanQR.hidden = false;
         canvasElement.hidden = true;
-      }
-    };
+    }
+};
